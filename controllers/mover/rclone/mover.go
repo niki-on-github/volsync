2853ddd295cce22314427a2e303678d4bc6bc27e
nix <nix@local>
2024-06-08 17:22:35 +0200

    try skif first backup wave

--- controllers/mover/rclone/mover.go
+++ controllers/mover/rclone/mover.go
@@ -231,6 +231,9 @@ func (m *Mover) ensureJob(ctx context.Context, dataPVC *corev1.PersistentVolumeC
 		if m.paused {
 			parallelism = int32(0)
 		}
+		if m.latestMoverStatus.Result == "" {
+			parallelism = int32(0)
+		}
 		job.Spec.Parallelism = &parallelism
 
 		envVars := []corev1.EnvVar{
